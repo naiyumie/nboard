07d368e2e100834a8e2ea6dd434428d04b6e9b83
naiyumie <naiyumie@gmail.com>
2015-03-02 23:22:22 +0900

    9

---
+++ ref/03.server-side_sql/nboard_mysql_user.sql
@@ -0,0 +1,12 @@
+# Privileges for `nboard`@`%`
+
+GRANT USAGE ON *.* TO 'nboard'@'%' IDENTIFIED BY PASSWORD '*35429BA3188F5C5F0DE63AEC69D1FCD03CDCC18C';
+
+GRANT ALL PRIVILEGES ON `nboard`.* TO 'nboard'@'%';
+
+
+# Privileges for `nboard`@`localhost`
+
+GRANT USAGE ON *.* TO 'nboard'@'localhost' IDENTIFIED BY PASSWORD '*35429BA3188F5C5F0DE63AEC69D1FCD03CDCC18C';
+
+GRANT ALL PRIVILEGES ON `nboard`.* TO 'nboard'@'localhost';
